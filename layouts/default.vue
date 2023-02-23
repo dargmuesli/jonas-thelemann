1bd382980bfb6fcc950e415a5982d4271f00485e
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-02-23 14:37:36 +0100

    feat(breadcrumbs): remove daisy-ui

--- layouts/default.vue
+++ layouts/default.vue
@@ -1,20 +1,7 @@
 <template>
   <div class="p-4 md:px-8">
-    <div class="breadcrumbs">
-      <Breadcrumbs>
-        <template #breadcrumb="{ to, title }">
-          <NuxtLink :to="localePath(to)">
-            {{ title }}
-          </NuxtLink>
-        </template>
-      </Breadcrumbs>
-    </div>
     <main>
       <slot />
     </main>
   </div>
 </template>
-
-<script setup lang="ts">
-const localePath = useLocalePath()
-</script>
