e0c72bc903da93d301b8527db28fc10511d744e7
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-02-27 09:32:42 +0100

    ci(cypress): improve test script

--- cypress/snapshots/test.sh
+++ cypress/snapshots/test.sh
@@ -1,22 +1,23 @@
 #!/bin/sh
 
-set -e
-
 THIS=$(dirname "$(readlink -f "$0")")
 
-if [ -d cypress/snapshots/diff ]; then
-  rm -rf cypress/snapshots/diff
+if [ -d "$THIS/diff" ]; then
+  rm -rf "$THIS/diff"
 fi
 
 pnpm cypress run --env type=actual
+ERROR=$?
 
-files=$(find cypress/snapshots/diff -type f || 'true')
+files=$(find "$THIS/diff" -type f || 'true')
 
 # if [ "$CI" != "" ]; then
 for file in $files; do
-  echo "@$THIS/../../$file"
-  curl -i -F file="@$THIS/../../$file" "https://tmpfiles.org/api/v1/upload"
+  echo "@$file"
+  curl -i -F file="@$file" "https://tmpfiles.org/api/v1/upload"
   printf "\n"
   break
 done
 # fi
+
+exit "$ERROR"
