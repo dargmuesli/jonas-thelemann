62ff9b5fc80fd0cdfc7d05c40e79d861ec092c91
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-04-14 03:44:48 +0200

    fix(vio): update to v1.8.2

---
+++ nuxt/app.vue
@@ -0,0 +1,10 @@
+<template>
+  <VioApp
+    :site-description="t('globalOgSeoDescription')"
+    :og-image-alt="t('globalOgImageAlt')"
+  />
+</template>
+
+<script setup lang="ts">
+const { t } = useI18n()
+</script>
