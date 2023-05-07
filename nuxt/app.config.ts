2e55385afd69a808a5c6c5df0fd4de8d7141c019
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-05-07 18:08:20 +0200

    feat(vio): update to v2.0.0-beta.1

--- nuxt/app.config.ts
+++ nuxt/app.config.ts
@@ -1,3 +1,43 @@
+const address = {
+  city: '34121 Kassel',
+  name: 'Jonas Thelemann',
+  street: 'Virchowstraße 4',
+}
+
 export default defineAppConfig({
+  legalNotice: {
+    contact: {
+      email: 'server+legal-notice@jonas-thelemann.de',
+    },
+    responsibility: {
+      address,
+    },
+    tmg: {
+      address,
+    },
+  },
+  privacyPolicy: {
+    hostingCdn: {
+      external: {
+        address: {
+          city: '91710 Gunzenhausen, Deutschland',
+          name: 'Hetzner Online GmbH',
+          street: 'Industriestr. 25',
+        },
+      },
+    },
+    mandatoryInfo: {
+      responsible: {
+        address: {
+          ...address,
+          email: 'server+privacy-policy@jonas-thelemann.de',
+        },
+      },
+    },
+  },
+  seoMeta: {
+    twitterSite: '@dargmuesli',
+  },
   siteName: 'Jonas Thelemann',
+  themeColor: '#202020',
 })
