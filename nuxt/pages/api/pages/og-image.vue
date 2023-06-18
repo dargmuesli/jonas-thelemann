68d025e7276c47b36133f92f960017d723ec3124
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-06-18 22:51:26 +0200

    feat(pages): move to api

--- nuxt/pages/api/pages/og-image.vue
+++ nuxt/pages/api/pages/og-image.vue
@@ -2,7 +2,7 @@
   <img
     alt="Open Graph image."
     fetchpriority="high"
-    :src="'/__og_image__/og.png'"
+    src="/__og_image__/og.png"
   />
   <!-- TODO: remove dynamic src string (https://github.com/nuxt/nuxt/issues/18920) -->
 </template>
