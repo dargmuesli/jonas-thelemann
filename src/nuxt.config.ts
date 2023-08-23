4a70972ce0ae07300ba1919ff22bc7f715b512db
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-23 03:32:01 +0200

    refactor(nuxt): outsource base configuration to vio

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,26 +1,17 @@
-import { I18N_MODULE_CONFIG } from '@dargmuesli/nuxt-vio/utils/constants'
+import { VIO_NUXT_BASE_CONFIG } from '@dargmuesli/nuxt-vio/utils/constants'
 
 import { SITE_NAME } from './utils/constants'
 
 export default defineNuxtConfig({
-  app: {
-    head: {
-      title: SITE_NAME, // fallback data to prevent invalid html at generation
-    },
-  },
+  ...VIO_NUXT_BASE_CONFIG({
+    siteName: SITE_NAME,
+  }),
   extends: ['@dargmuesli/nuxt-vio'],
 
-  // modules
-  i18n: {
-    ...I18N_MODULE_CONFIG, // `langDir`, `lazy` and `locales` must be configured to extend a layer having lazy-loaded translations (https://v8.i18n.nuxtjs.org/guide/layers#locales)
-  },
   linkChecker: {
     excludeLinks: [
       'https://www.linkedin.com/in/jonas-thelemann-148a74205/', // requires login
     ],
     failOnError: false, // TODO: enable (https://github.com/harlan-zw/nuxt-link-checker/issues/13)
-  },
-  site: {
-    name: SITE_NAME,
   },
 })
