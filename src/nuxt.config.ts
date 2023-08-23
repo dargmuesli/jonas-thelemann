085ba28182413bc438999762bc556d91ef4253b4
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-08-23 06:10:02 +0200

    fix(nuxt): properly extend configuration

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -1,17 +1,22 @@
 import { VIO_NUXT_BASE_CONFIG } from '@dargmuesli/nuxt-vio/utils/constants'
+import { defu } from 'defu'
 
 import { SITE_NAME } from './utils/constants'
 
-export default defineNuxtConfig({
-  ...VIO_NUXT_BASE_CONFIG({
-    siteName: SITE_NAME,
-  }),
-  extends: ['@dargmuesli/nuxt-vio'],
+export default defineNuxtConfig(
+  defu(
+    {
+      extends: ['@dargmuesli/nuxt-vio'],
 
-  linkChecker: {
-    excludeLinks: [
-      'https://www.linkedin.com/in/jonas-thelemann-148a74205/', // requires login
-    ],
-    failOnError: false, // TODO: enable (https://github.com/harlan-zw/nuxt-link-checker/issues/13)
-  },
-})
+      linkChecker: {
+        excludeLinks: [
+          'https://www.linkedin.com/in/jonas-thelemann-148a74205/', // requires login
+        ],
+        failOnError: false, // TODO: enable (https://github.com/harlan-zw/nuxt-link-checker/issues/13)
+      },
+    },
+    VIO_NUXT_BASE_CONFIG({
+      siteName: SITE_NAME,
+    }),
+  ),
+)
