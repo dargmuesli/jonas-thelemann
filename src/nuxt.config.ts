738e9f857bd748ffcdc21c96f6b040ba2f29d83a
Jonas Thelemann <e-mail@jonas-thelemann.de>
2023-09-22 03:19:11 +0200

    chore(link-checker): disable uni-kassel on ci

--- src/nuxt.config.ts
+++ src/nuxt.config.ts
@@ -11,7 +11,12 @@ export default defineNuxtConfig(
       // modules
       linkChecker: {
         excludeLinks: [
-          ...(process.env.CI ? ['https://www.instagram.com/dargmuesli/'] : []),
+          ...(process.env.CI
+            ? [
+                'https://www.instagram.com/dargmuesli/',
+                'https://www.uni-kassel.de/',
+              ]
+            : []),
           'https://www.linkedin.com/in/jonas-thelemann-148a74205/', // requires login
         ],
       },
