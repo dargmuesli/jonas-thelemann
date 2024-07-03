c9f84e6667b2a51f8a9465d7b860acaaffd8ffe1
Jonas Thelemann <e-mail@jonas-thelemann.de>
2024-07-03 03:48:03 +0200

    fix(sentry): correct url

--- src/.config/nuxt.ts
+++ src/.config/nuxt.ts
@@ -58,7 +58,7 @@ export default defineNuxtConfig(
               ],
               'report-to': 'csp-endpoint',
               'report-uri':
-                'https://o4507259039973376.ingest.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
+                'https://o4507259039973376.ingest.de.sentry.io/api/4507260561653840/security/?sentry_key=1e53178c1dba9b39147de4a21853a3e3',
               'script-src-attr': "'unsafe-inline'", // Nuxt image on error
             },
             {
