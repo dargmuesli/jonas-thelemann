cdda3721b4fa973a18e0720bbe91f6d3035f13df
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-01-08 14:30:38 +0100

    feat(pages): update picture

--- src/app/components/OgImage/Default.vue
+++ src/app/components/OgImage/Default.vue
@@ -3,16 +3,18 @@
     class="flex h-full w-full flex-nowrap bg-[linear-gradient(to_bottom,#dbf4ff,#fff1f1)]"
   >
     <img
-      alt="Jonas in Tutzing."
-      src="/assets/static/images/tutzing.jpg"
+      alt="Jonas in Wiesbaden."
+      class="object-cover"
+      src="/assets/static/images/wiesbaden.jpg"
       width="436"
       height="630"
     />
-    <span class="mx-16 flex flex-1 flex-col justify-center">
-      <h1 class="truncate text-7xl">
+    <span class="mx-16 flex flex-1 flex-col justify-center gap-16">
+      <!-- eslint-disable-next-line prettier/prettier -->
+      <h1 class="truncate leading-[1.1875] text-7xl">
         {{ title }}
       </h1>
-      <div class="mt-16 text-3xl text-gray-600">
+      <div class="text-3xl text-gray-600">
         {{ description }}
       </div>
     </span>
