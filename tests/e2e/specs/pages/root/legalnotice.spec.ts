76a8bdfa5963276b0a99c2e167bd7ea257d19c86
Jonas Thelemann <e-mail@jonas-thelemann.de>
2026-03-06 05:24:21 +0100

    feat(seo): switch from satori to takumi

--- tests/e2e/specs/pages/root/legalnotice.spec.ts
+++ tests/e2e/specs/pages/root/legalnotice.spec.ts
@@ -10,7 +10,13 @@ const PAGE_PATH = '/legal-notice'
 testA11y(PAGE_PATH)
 testPageLoad(PAGE_PATH)
 testOgImage({
-  de: 'a_Ein+Bild+von+Jonas+Thelemann+und+der+Titel+der+Seite.,c_Default.satori,description_Portfolio+von+Jonas+Thelemann.,title_Impressum,p_Ii9kZS9sZWdhbC1ub3RpY2Ui.png',
-  en: `a_A+picture+of+Jonas+Thelemann+and+the+title+of+the+page.,c_Default.satori,description_Jonas+Thelemann's+portfolio.,title_Legal+notice,p_Ii9sZWdhbC1ub3RpY2Ui.png`,
+  dynamic: {
+    de: 'a_Ein+Bild+von+Jonas+Thelemann+und+der+Titel+der+Seite.,c_Default.takumi,description_Portfolio+von+Jonas+Thelemann.,title_Impressum,p_Ii9kZS9sZWdhbC1ub3RpY2Ui.png',
+    en: `a_A+picture+of+Jonas+Thelemann+and+the+title+of+the+page.,c_Default.takumi,description_Jonas+Thelemann's+portfolio.,title_Legal+notice,p_Ii9sZWdhbC1ub3RpY2Ui.png`,
+  },
+  static: {
+    de: 'a_Ein+Bild+von+Jonas+Thelemann+und+der+Titel+der+Seite.,c_Default.takumi,description_Portfolio+von+Jonas+Thelemann.,title_Impressum,p_Ii9kZS9sZWdhbC1ub3RpY2Ui.png',
+    en: `a_A+picture+of+Jonas+Thelemann+and+the+title+of+the+page.,c_Default.takumi,description_Jonas+Thelemann's+portfolio.,title_Legal+notice,p_Ii9sZWdhbC1ub3RpY2Ui.png`,
+  },
 })
 testVisualRegression(PAGE_PATH)
